f2b8ad1e557e22f1a5c8dc522faac93469235cbc
Wyddenberg Thallys <wthallys.02@gmail.com>
2024-07-22 16:13:15 -0300

    refactor: organize routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,9 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Login from "./pages/Login";
-import SignUp from "./pages/SignUp";
-import NotFound from "./pages/NotFound";
-import QrCode from "./pages/QrCode";
-import EditLinks from "./pages/EditLinks";
-import PublicProfile from "./pages/PublicProfile";
-import PrivateRoute from "./utils/PrivateRoute";
+import AppRoutes from "./routes/AppRoutes";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="signup" element={<SignUp />} />
-        <Route path=":userIdUrl" element={<PublicProfile />} />
-        <Route
-          path="user/:userIdUrl"
-          element={
-            <PrivateRoute>
-              <QrCode />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="user/:userIdUrl/edit"
-          element={
-            <PrivateRoute>
-              <EditLinks />
-            </PrivateRoute>
-          }
-        />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <AppRoutes />
   );
 }
 
---
+++ src/routes/AppRoutes.tsx
@@ -0,0 +1,39 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "../pages/Login";
+import SignUp from "../pages/SignUp";
+import PublicProfile from "../pages/PublicProfile";
+import PrivateRoute from "../utils/PrivateRoute";
+import QrCode from "../pages/QrCode";
+import NotFound from "../pages/NotFound";
+import EditLinks from "../pages/EditLinks";
+
+const AppRoutes = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="signup" element={<SignUp />} />
+        <Route path=":userIdUrl" element={<PublicProfile />} />
+        <Route
+          path="user/:userIdUrl"
+          element={
+            <PrivateRoute>
+              <QrCode />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="user/:userIdUrl/edit"
+          element={
+            <PrivateRoute>
+              <EditLinks />
+            </PrivateRoute>
+          }
+        />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default AppRoutes;
